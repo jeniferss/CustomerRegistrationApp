50635e8df0f4ea23d1742ccdd296e710357368b7
jenifers-s <67614296+jeniferss@users.noreply.github.com>
2022-09-07 00:19:42 -0300

    add Docker and README

---
+++ README.md
@@ -0,0 +1,24 @@
+# Client Register Django App
+
+[![made-with-python](https://img.shields.io/badge/Made%20with-Python-1f425f.svg)](https://www.python.org/)
+> Django simple app for client registration
+
+## How to Use
+
+---
+To clone and run this application, you'll need Git, Python and Docker installed on your computer. From your command line:
+
+```sh
+# Clone this repository
+$ git clone https://github.com/jeniferss/client.git
+# Go into the repository
+$ cd client
+# Run migrations
+$ python manage.py runserver
+# Create superuser
+$ python mange.py createsuperuser
+# Run container
+$ docker-compose up # or run app with python manage.py runserver
+
+# Server will start at http://localhost:8000
+```
--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -28,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'client'
+    'client'
 ]
 
 MIDDLEWARE = [
@@ -71,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -81,7 +78,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -101,20 +97,18 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
 LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'utc'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'

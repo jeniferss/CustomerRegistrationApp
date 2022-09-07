90b96a124c9275de4afa72ca730a3b5534d9046e
jenifers-s <67614296+jeniferss@users.noreply.github.com>
2022-09-06 23:27:36 -0300

    reafactor Templates | Functions to Class based Views | Form Validators

--- config/urls.py
+++ config/urls.py
@@ -1,33 +1,19 @@
-"""config URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
-from client import views
-from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
+
+from client.views.client_list import ClientListView
+from client.views.client_register import ClientRegisterView
+from client.views.home import HomeView
+from client.views.login import LoginView
+from client.views.logout import logout_user
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-	path('index/client_list/', views.list_clients),
-	path('', RedirectView.as_view(url='/index/')),
-	path('login/', views.login_user),
-	path('login/submit', views.submit_login),
-	path('logout/', views.logout_user),
-	path('index/clientes/', views.clientes),
-	path('index/clientes/submit', views.submit_clientes),
-	path('index/', views.index),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+                  path('admin/', admin.site.urls),
+                  path('client/list/', ClientListView.as_view()),
+                  path('client/register/', ClientRegisterView.as_view()),
+                  path('', HomeView.as_view()),
+                  path('login/', LoginView.as_view()),
+                  path('logout/', logout_user)
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
